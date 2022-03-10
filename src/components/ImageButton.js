23fbb8592404f612d6ee03b119ff5ce1a559b68d
cakwhdtj <48639861+cakwhdtj@users.noreply.github.com>
2022-03-10 15:49:36 +0900

    TypeComponent
    send btn

--- src/components/ImageButton.js
+++ src/components/ImageButton.js
@@ -5,24 +5,18 @@ import store from '../index'
 import { connect,useSelector } from 'react-redux'
 import { addTochatdata_input, addTochatdata_output,addVideo,setImageNum } from "../redux_src/chat_rdx";
 import axios from 'axios'
-class ImageButton extends Component
-{
-    render()
-    {
+class ImageButton extends Component {
+    render() {
         var id=this.props._id
         var imgsrc= this.props._imgsrc
         var port = this.props._port
         var url = this.props._url
         return(
-            <Link to='/main'
-            
+            <Link to='/main'   
             onClick={()=>{store.dispatch(setImageNum(id))
-
                 axios.post(url+':'+port+'/prepare',{'image_num':store.getState()['image']})
-
                 .then(function(response) {
                     console.log(response)
-
                 }
                 )
             }}
@@ -33,10 +27,7 @@ class ImageButton extends Component
                     objectFit:'fill',
                     width:'300px',
                     height:'300px',
-                    
-
                 }}></img>
-               
             </Link>
         )
     }
--- src/components/MainComponent/Typing/TypeComponent.js
+++ src/components/MainComponent/Typing/TypeComponent.js
@@ -4,18 +4,33 @@ import {useState} from 'react'
 import axios from "axios"
 import font from '../../font.css'
 
-const TypeComponent=()=>
-{
-    const [ins, setins] = useState();
-    const sendMessage=()=>
-{
-    //axios.post()
-    setins("")
 
-}
+const TypeComponent = () => {
+    const [ins, setins] = useState();
+    const [btnOn,setBtn] = useState(false);
+    const [focus, isFocus] = useState(false);
+
+    const sendMessage=()=> {
+        //axios.post()
+        setins("")
+    }
+    const _onChange=(e)=>{
+        isFocus(true);
+        setins(e.target.value);
+    }
+    const _onBlur=()=>{
+        if (focus === true && ins != "") {
+        } else {
+            setBtn(false);
+            isFocus(false);
+        }
+    }
+    const btnClick = () => {
+        sendMessage();
+        document.getElementById("talk_input").focus();
+    }
 
     return(
-        
         <div
         style={{
             position:'absolute',
@@ -30,11 +45,16 @@ const TypeComponent=()=>
             color:'white',
         }}>
             <input 
+            autoComplete="off"
             onKeyPress={(e)=>{if(e.key=='Enter'){sendMessage()}}}
-            onChange={(e)=>{setins(e.target.value)}}
+            onChange={_onChange}
+            onFocus={()=>setBtn(true)}
+            onBlur={_onBlur}
             value={ins}
             placeholder='Type Something'
-            className="input" style={{
+            className="input" 
+            id="talk_input"
+            style={{
                 fontFamily:'Roboto',
                 backgroundColor:'rgba(0,0,0,0)',
                 border:'0px solid black',
@@ -42,6 +62,19 @@ const TypeComponent=()=>
                 fontSize:'1.2vw',
             }}>
             </input>
+            {(btnOn === true) ? 
+                (<button style={{
+                    background: 'none',
+                    color: 'inherit',
+                    border: '1px solid red',
+                    border: 'none',
+                    padding: '0',
+                    font: 'inherit',
+                    cursor: 'pointer',
+                    outline: 'inherit',
+                }}  
+                onClick={btnClick}><img src={require('../../../image/Vector.png').default}></img></button>)
+            : null}
         </div>
     )
 }
